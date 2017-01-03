1bc9de5a6f47544f58bce88cc884eefe349de18d
Mateus de Campos <mateuscampos_nba@hotmail.com>
2017-01-03 20:21:07 -0200

    adding controllers and cell, changing launch screen

---
+++ LearningShotUIKit/LearningShotUIKit/UICollectionViewCustonCell.swift
@@ -0,0 +1,13 @@
+//
+//  UICollectionViewCustonCell.swift
+//  LearningShotUIKit
+//
+//  Created by Mateus de Campos on 03/01/17.
+//  Copyright © 2017 Mateus Campos. All rights reserved.
+//
+
+import UIKit
+
+class UICollectionViewCustonCell: UICollectionViewCell {
+    
+}
